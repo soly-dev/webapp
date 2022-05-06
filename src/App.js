7345d47fdf42dfa7139209b5deebacc09857ba3c
soly-dev <soly.im.dev@gmail.com>
2022-05-06 15:17:21 -0400

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+  const [hide,setshow]=React.useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Press the botton to see new text</h1>
+      {
+        hide?<h2>A bunch of new text</h2>:null
+      }
+      <button onClick={() => setshow(true)}>Press Me</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import App from './App';
+
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Press the botton to see new text");
   expect(linkElement).toBeInTheDocument();
+});
+Enzyme.configure({ adapter: new Adapter() });
+
+// Testing to see if the new text show after button clicked
+describe('Test Button component', () => {
+ it('should show text when a button is clicked', () => {
+   const wrapper = shallow(<App />);
+   const button = wrapper.find('button');
+   button.simulate('click');
+   const text = wrapper.find('div h2');
+   expect(text.text()).toBe('A bunch of new text');
+ })
 });
